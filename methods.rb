913d275c98bc9c43c1ee94ecb5442929d0a9bdb9
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2022-07-25 13:22:30 -0400

    commit tree 93f2f3cd2bd94304d81b0923eff56e5f13c7cae2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+greet_programmer
+
+
+
+def greet(name)
+    puts ("Hello"+name)
+end 
+greet("Naureen")
+greet("Jimmy")
+
+
+    def add (num1,num2)
+        puts num1
+        puts num2
+        num1 +num2 
+    end
+    puts add(2) + add(5)
+
